2c07169a9ccc1f0a978df71195b851011a4a45e2
Tushar <33440945+tnandwani@users.noreply.github.com>
2022-02-16 09:52:27 -0800

    fixed token check?

--- components/Main.jsx
+++ components/Main.jsx
@@ -83,7 +83,9 @@ export default function Main() {
         // If mobile
         if (Platform.OS !== 'web') {
             registerForPushNotificationsAsync().then(token => {
-                addUsertoZone(token)
+                if (token){
+                    addUsertoZone(token)
+                }
             }).catch((error) => {
                 sendFireError(error.message, "MAIN.registerForPushNotificationsAsync")
             });
