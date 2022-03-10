1140e24d10b40cc8702654cfc4fe77f4828dab83
Tushar <33440945+tnandwani@users.noreply.github.com>
2022-03-10 13:24:17 -0800

    added embeded browser

--- components/widgets/Resources.jsx
+++ components/widgets/Resources.jsx
@@ -1,23 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, {  } from "react";
 
 // UI
 import {
     Box,
     Center,
-    VStack,
-    Flex,
-    Spacer,
     HStack,
     Pressable,
 
-    Heading,
-
 } from "native-base";
-
-
-import { MaterialIcons } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import * as Linking from 'expo-linking'
+import * as WebBrowser from 'expo-web-browser';
 
 export default function Resources(props) {
 
@@ -26,7 +18,7 @@ export default function Resources(props) {
         <Box>
             <HStack space={3} justifyContent="center">
                 {/* TRAINING */}
-                <Pressable w="30%" onPress={() => { Linking.openURL('https://www.akc.org/expert-advice/training/basic-training/') }}>
+                <Pressable w="30%" onPress={() => { WebBrowser.openBrowserAsync('https://www.akc.org/expert-advice/training/basic-training/') }}>
                     <Center bg="amber.400" _text={{ color: "white" }} rounded="md" shadow={3} py='2' >
                         <MaterialCommunityIcons
                             name="dog-service"
@@ -38,7 +30,7 @@ export default function Resources(props) {
                 </Pressable>
 
                 {/* HEALTH */}
-                <Pressable w="30%" onPress={() => { Linking.openURL('https://www.akc.org/expert-advice/health/') }}>
+                <Pressable w="30%" onPress={() => { WebBrowser.openBrowserAsync('https://www.akc.org/expert-advice/health/') }}>
                     <Center bg="danger.400" _text={{ color: "white" }} rounded="md" shadow={3} py='2' >
                         <MaterialCommunityIcons
                             name="stethoscope"
@@ -50,7 +42,7 @@ export default function Resources(props) {
                 </Pressable>
 
                 {/* DIET */}
-                <Pressable w="30%" onPress={() => { Linking.openURL('https://www.akc.org/expert-advice/nutrition/') }}>
+                <Pressable w="30%" onPress={() => { WebBrowser.openBrowserAsync('https://www.akc.org/expert-advice/nutrition/') }}>
 
                     <Center bg="emerald.400" _text={{ color: "white" }} rounded="md" shadow={3} py='2' >
                         <MaterialCommunityIcons
